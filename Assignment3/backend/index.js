e11d7e9eec6e588869f60231d8bc3bc7638b1bef
CHSherr <110822398+CHSherr@users.noreply.github.com>
2024-10-31 16:06:27 -0700

    finish the day view tab and add some imgs

---
+++ Assignment3/backend/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get("/nextweek", async (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  const url =
+    "https://api.tomorrow.io/v4/timelines?location=" +
+    lat +
+    "%2C" +
+    lon +
+    "&fields=temperatureMax&fields=temperatureMin&fields=weatherCode&fields=windSpeed&fields=sunsetTime&fields=sunriseTime&fields=humidity&fields=visibility&fields=cloudCover&fields=temperatureApparent&units=imperial&timesteps=1d&startTime=now&endTime=nowPlus5d&apikey=5SfaEn9QUMQGU8Szu302sjx67rg2R6Tp";
+
+  try {
+    const response = await axios.get(url);
+    let intervals = response.data.data.timelines[0].intervals;
+    for (const interval of intervals) {
+      interval.startTime = new Date(interval.startTime).toLocaleDateString(
+        "en-US",
+        { weekday: "long", month: "short", day: "numeric", year: "numeric" }
+      );
+      interval.values.sunriseTime = new Date(
+        interval.values.sunriseTime
+      ).toLocaleTimeString("en-US", { hour: "numeric", hour12: true });
+      interval.values.sunsetTime = new Date(
+        interval.values.sunsetTime
+      ).toLocaleTimeString("en-US", { hour: "numeric", hour12: true });
+    }
+    res.send(intervals);
+  } catch {
+    console.error("Error in fetching data from Tomorrow API");
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- Assignment3/src/app/search-form/search-form.component.ts
+++ Assignment3/src/app/search-form/search-form.component.ts
@@ -1,16 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.css',
 })
 export class SearchFormComponent {
   requiredFields = true;
+  nextWeek: any[] = [];
+  address: string = '';
+  weatherCodes: { [key: number]: string[] | string } = {
+    0: 'Unknown',
+    1000: ['Clear', '/assets/weatherCode/clear_day.svg'],
+    1100: ['Mostly Clear', '/assets/weatherCode/mostly_clear_day.svg'],
+    1101: ['Partly Cloudy', '/assets/weatherCode/partly_cloudy_day.svg'],
+    1102: ['Mostly Cloudy', '/assets/weatherCode/mostly_cloudy.svg'],
+    1001: ['Cloudy', '/assets/weatherCode/cloudy.svg'],
+    2000: ['Fog', '/assets/weatherCode/fog.svg'],
+    2100: ['Light Fog', '/assets/weatherCode/fog_light.svg'],
+    4000: ['Drizzle', '/assets/weatherCode/drizzle.svg'],
+    4001: ['Rain', '/assets/weatherCode/rain.svg'],
+    4200: ['Light Rain', '/assets/weatherCode/rain_light.svg'],
+    4201: ['Heavy Rain', '/assets/weatherCode/rain_heavy.svg'],
+    5000: ['Snow', '/assets/weatherCode/snow.svg'],
+    5001: ['Flurries', '/assets/weatherCode/flurries.svg'],
+    5100: ['Light Snow', '/assets/weatherCode/snow_light.svg'],
+    5101: ['Heavy Snow', '/assets/weatherCode/snow_heavy.svg'],
+    6000: ['Freezing Drizzle', '/assets/weatherCode/freezing_drizzle.svg'],
+    6001: ['Freezing Rain', '/assets/weatherCode/freezing_rain.svg'],
+    6200: [
+      'Light Freezing Rain',
+      '/assets/weatherCode/freezing_rain_light.svg',
+    ],
+    6201: [
+      'Heavy Freezing Rain',
+      '/assets/weatherCode/freezing_rain_heavy.svg',
+    ],
+    7000: ['Ice Pellets', '/assets/weatherCode/ice_pellets.svg'],
+    7101: ['Heavy Ice Pellets', '/assets/weatherCode/ice_pellets_heavy.svg'],
+    7102: ['Light Ice Pellets', '/assets/weatherCode/ice_pellets_light.svg'],
+    8000: ['Thunderstorm', '/assets/weatherCode/tstorm.svg'],
+  };
+  activeIndex: number = 1;
+
+  @ViewChild('danger') danger!: ElementRef;
+
+  setActiveTab(index: number): void {
+    this.activeIndex = index;
+  }
 
   onSubmit(form: any) {
     if (this.requiredFields) {
@@ -18,7 +60,7 @@ export class SearchFormComponent {
       const city = form.value.city;
       const state = form.value.state;
       const country = form.value.country;
-      let lat, lon, location;
+      let lat, lon;
       const google_url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city},${state},${country}&key=AIzaSyDqXJTP92xb2T3PC2fq0bGCIJmF68Y-vyY`;
 
       fetch(google_url)
@@ -27,21 +69,30 @@ export class SearchFormComponent {
           // console.log(data);
           lat = data.results[0].geometry.location.lat;
           lon = data.results[0].geometry.location.lng;
-          location = data.results[0].formatted_address;
-          console.log(lat, lon, location);
+          this.address = data.results[0].formatted_address;
+          console.log(lat, lon, this.address);
+          this.getNextWeek(lat, lon);
         });
     } else {
       const ip_url = 'https://ipinfo.io/?token=b72b65292cdc46';
-      let lat, lon, location;
+      let lat, lon;
 
       fetch(ip_url)
-        .then((response) => response.json())
+        .then((response) => {
+          if (response.status !== 200) {
+            this.danger.nativeElement.style.display = 'block';
+            return Promise.reject('Non-200 status code');
+          } else {
+            return response.json();
+          }
+        })
         .then((data) => {
           // console.log(data);
           lat = data.loc.split(',')[0];
           lon = data.loc.split(',')[1];
-          location = data.city + ', ' + data.region;
-          console.log(lat, lon, location);
+          this.address = data.city + ', ' + data.region;
+          console.log(lat, lon, this.address);
+          this.getNextWeek(lat, lon);
         });
     }
   }
@@ -113,5 +164,30 @@ export class SearchFormComponent {
       const event = new Event('change');
       checkbox.dispatchEvent(event);
     }
+  }
+
+  onClick(
+    active: HTMLAnchorElement,
+    inactive: HTMLAnchorElement,
+    visible: HTMLDivElement,
+    hidden: HTMLDivElement
+  ) {
+    if (!active.classList.contains('active')) {
+      active.classList.add('active');
+      inactive.classList.remove('active');
+      visible.style.display = 'flex';
+      hidden.style.display = 'none';
+    }
+  }
+
+  getNextWeek(lat: number, lon: number) {
+    const weather_url =
+      'http://localhost:3000/nextweek?lat=' + lat + '&lon=' + lon;
+    fetch(weather_url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.nextWeek = data;
+        console.log(this.nextWeek);
+      });
   }
 }
