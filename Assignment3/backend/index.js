12b43b9fca98b8ee9cab34348112f5a6fccd8844
CHSherr <110822398+CHSherr@users.noreply.github.com>
2024-10-31 20:11:46 -0700

    finish the most part of the view tabs still need to style a little bit

--- Assignment3/backend/index.js
+++ Assignment3/backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.get("/nextweek", async (req, res) => {
   const lat = req.query.lat;
   const lon = req.query.lon;
-  const url =
+  let url =
     "https://api.tomorrow.io/v4/timelines?location=" +
     lat +
     "%2C" +
@@ -34,6 +34,26 @@ app.get("/nextweek", async (req, res) => {
         interval.values.sunsetTime
       ).toLocaleTimeString("en-US", { hour: "numeric", hour12: true });
     }
+    res.send(intervals);
+  } catch {
+    console.error("Error in fetching data from Tomorrow API");
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/hourly", async (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  let url =
+    "https://api.tomorrow.io/v4/timelines?location=" +
+    lat +
+    "%2C" +
+    lon +
+    "&fields=temperature&fields=humidity&fields=pressureSeaLevel&fields=windSpeed&fields=windDirection&fields=temperature&units=imperial&timesteps=1h&startTime=now&endTime=nowPlus5d&apikey=5SfaEn9QUMQGU8Szu302sjx67rg2R6Tp";
+
+  try {
+    const response = await axios.get(url);
+    let intervals = response.data.data.timelines[0].intervals;
     res.send(intervals);
   } catch {
     console.error("Error in fetching data from Tomorrow API");
